6f6e448b5f71912823c557b1d94d5a2b044d392c
vkolesnev <admin@admin.com>
2021-02-10 15:54:16 +0700

    chore: fix

--- js/form.js
+++ js/form.js
@@ -98,6 +98,7 @@ var FormReBuild = (function () {
 
                             let sent = false;
                             jQuery.each(bitrixEvents.LIST, function (i, bitrixEvent) {
+                                console.log(userId);
                                 if(bitrixEvent.EVENT_TYPE === eventName &&
                                     parseInt(bitrixEvent.USER_ID) === parseInt(userId) ){
                                     sent = true;
