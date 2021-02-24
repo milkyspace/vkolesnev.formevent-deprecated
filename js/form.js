89405c44b0932d6645a336b78107e91070e43aa2
vkolesnev <admin@admin.com>
2021-02-24 12:01:35 +0700

    chore: console.log

--- js/form.js
+++ js/form.js
@@ -26,6 +26,7 @@ var FormReBuild = (function () {
                     eventName = rule.UF_EVENT_NAME;
 
                     console.log(button)
+                    console.log(event)
 
                     if(rule.UF_BUTTON_SELECTOR.length) {
                         console.log(event)
