233ce4b83c18ebb5f560793f8706680623018b18
vkolesnev <admin@admin.com>
2021-02-24 11:05:33 +0700

    chore: console.log

--- js/form.js
+++ js/form.js
@@ -1,4 +1,5 @@
 'use strict';
+
 var FormReBuild = (function () {
     let birixEvents = [];
     let userId;
@@ -22,8 +23,8 @@ var FormReBuild = (function () {
                     form = jQuery(rule.UF_FORM_SELECTOR);
                     event = rule.UF_EVENT;
                     eventName = rule.UF_EVENT_NAME;
-
                     button.click(function () {
+                        console.log(`event ${event} init`);
                         form.attr('form-event', event);
                     });
                     form.submit(function () {
@@ -60,69 +61,7 @@ var FormReBuild = (function () {
 })();
 
 jQuery(function () {
+    console.log('dd');
     // FormReBuild.getUserId();
-    FormReBuild.getList();
-});
-'use strict';
-var FormReBuild = (function () {
-    let birixEvents = [];
-    let userId;
-
-    var getUserId = function () {
-        FormEvent.getUserId().then(function (result) {
-            userId = result.USER_ID;
-        });
-    };
-
-    var getList = function () {
-        FormEvent.getList().then(function (result) {
-            if (result.LIST) {
-                let event;
-                let button;
-                let form;
-                let eventName;
-                let userId = result.USER_ID;
-                jQuery.each(result.LIST, function (i, rule) {
-                    button = jQuery(rule.UF_BUTTON_SELECTOR);
-                    form = jQuery(rule.UF_FORM_SELECTOR);
-                    event = rule.UF_EVENT;
-                    eventName = rule.UF_EVENT_NAME;
-
-                    button.click(function () {
-                        form.attr('form-event', event);
-                    });
-                    form.submit(function () {
-                        let $thisForm = jQuery(this);
-                        FormEvent.getBitrixEvents().then(function (bitrixEvents) {
-                            let formEvent = $thisForm.attr('form-event');
-
-                            let sent = false;
-                            jQuery.each(bitrixEvents.LIST, function (i, bitrixEvent) {
-                                if(bitrixEvent.EVENT_TYPE === eventName &&
-                                    parseInt(bitrixEvent.USER_ID) === parseInt(userId) ){
-                                    sent = true;
-                                }
-                            });
-
-                            if(sent){
-                                console.log(`add event ${formEvent}`);
-                                dataLayer.push({'event': formEvent});
-                            } else{
-                                console.log(`event ${formEvent} didn't sent`);
-                            }
-
-                        });
-                    });
-                });
-            }
-        });
-    };
-
-    return {
-        getList: getList
-    };
-})();
-
-jQuery(function () {
     FormReBuild.getList();
 });
